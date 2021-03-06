1b06b85a9b069a19554bd743cbf3232f546dc0cf
Mino <mino9421@gmail.com>
2021-03-05 21:50:49 -0500

    Update Queue.java

--- stack_queue/Queue.java
+++ stack_queue/Queue.java
@@ -8,7 +8,7 @@ public class Queue {
     }
 
     void enQueue(int key) {
-        Node n = new Node(key);
+        Node n = new Node(key);  // new item enters the queue in the last spot
         if(head == null) {
             head = n;
             return;
@@ -20,23 +20,23 @@ public class Queue {
         curr.next = n;
     }
 
-    int deQueue() {
+    int deQueue() { // if queue exists then deQueue first item
         if(head == null) {
             return -1000000;
         }
         Node curr = head;
         head = head.next;
-        return curr.data;
+        return curr.data; // return the next item to be deQueued
     }
 
     boolean isEmpty() {
-        return head == null;
+        return head == null; // this will return boolean
     }
 
     int peek() {
         if(head == null) {
             return -10000000;
         }
-        return head.data;
+        return head.data;  // will return the data if it exists
     }   
 }
