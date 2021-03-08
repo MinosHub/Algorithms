68f0fa7ea13f5d4a86cd589cfdadc64a5301ff8e
Mino <mino9421@gmail.com>
2021-03-08 05:24:30 -0500

    added  "misc" Directory
    moved collatz and factorial into the dir

--- Quick.java
+++ Quick.java
@@ -52,7 +52,7 @@ public class Quick {
     }
 
     public static void main(String[] args) {
-        int arr[] = {10, 7, 8, 9, 1, 5}; 
+        int arr[] = { 64,-6,44,25,37,7,29,-22,12,-18 };
         int n = arr.length; 
   
         System.out.println("Unsorted array"); 
