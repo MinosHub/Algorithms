d5942dcc946343ea455ae2ba77502e446b71c239
Mino <mino9421@gmail.com>
2021-03-09 12:44:01 -0500

    line modifications

--- Hashing/LinearHashTable.java
+++ Hashing/LinearHashTable.java
@@ -12,77 +12,77 @@ public class LinearHashTable {
     
     public LinearHashTable(int max,double lf){
         table = new String[max];
-        tableSize= max;
-        for (int x = 0; x<tableSize; x++){
-            table[x]=EMPTY;
+        tableSize = max;
+        for (int x = 0; x < tableSize; x++){
+            table[x] = EMPTY;
         }
-        loadFactor=lf;
-        numItems=0;
+        loadFactor = lf;
+        numItems = 0;
     }
     public LinearHashTable(int max){
         table = new String[max];
-        tableSize= max;
-        for (int x = 0; x<tableSize; x++){
-            table[x]=EMPTY;
+        tableSize = max;
+        for (int x = 0; x < tableSize; x++){
+            table[x] = EMPTY;
         }
-        loadFactor=0.75;
-        numItems=0;
+        loadFactor = 0.75;
+        numItems = 0;
     }
     
     public LinearHashTable(){
         table = new String[20];
-        tableSize= 20;
-        for (int x = 0; x<tableSize; x++){
-            table[x]=EMPTY;
+        tableSize = 20;
+        for (int x = 0; x < tableSize; x++){
+            table[x] = EMPTY;
         }
-        loadFactor=0.75;
-        numItems=0;
+        loadFactor = 0.75;
+        numItems = 0;
     }
     
     private int hashFunction(String word){
-        int value=0,weight=1;
-        for (int x =0; x<word.length();x++){
-            value += (word.charAt(x)-'a'+1)*weight;
+        int value = 0, weight = 1;
+        for (int x = 0; x < word.length(); x++){
+            value += (word.charAt(x) - 'a' + 1) * weight;
             weight++;
         }
-        return value%tableSize;
+        return value % tableSize;
     }
     // insert search delete (update)
     public void insert (String word){
-        if (numItems/tableSize < loadFactor){
-            int loc =hashFunction(word);  
-            while (table[loc].compareTo(EMPTY)!=0 && table[loc].compareTo("DELETED")!=0){
-                loc = (loc+1)%tableSize;
-            }    
-            table[loc]=word;
+        if (numItems / tableSize < loadFactor){
+            int loc = hashFunction(word);  
+            while (table[loc].compareTo(EMPTY) != 0 && table[loc].compareTo("DELETED") != 0){
+                loc = (loc + 1) % tableSize;
+            }
+            table[loc] = word;
             numItems++;
         }
     }
     
     public int search(String word){
         int loc = hashFunction(word);  
-        while (table[loc].compareTo(word)!=0 && table[loc].compareTo(EMPTY)!=0){
-            loc = (loc+1)%tableSize;
+        while (table[loc].compareTo(word) != 0 && table[loc].compareTo(EMPTY) != 0){
+            loc = (loc + 1) % tableSize;
         }
-        if (table[loc].compareTo(EMPTY)==0)
+        if (table[loc].compareTo(EMPTY) == 0)
             return -1;
         return loc;
     }
     
     public void delete(String word){
         int loc = hashFunction(word);  
-        while (table[loc].compareTo(word)!=0 && table[loc].compareTo(EMPTY)!=0){
-            loc = (loc+1)%tableSize;
+        while (table[loc].compareTo(word) != 0 && table[loc].compareTo(EMPTY) != 0){
+            loc = (loc + 1) % tableSize;
         }
-        if (table[loc].compareTo(EMPTY)!=0){
-            table[loc]="DELETED"; 
+        if (table[loc].compareTo(EMPTY) != 0){
+            table[loc] = "DELETED"; 
             numItems--;
         }
     } 
     
     public void printTable(){
         System.out.println("Hash Table Contents");
-        for (int x=0;x<tableSize;x++){
+        for (int x = 0; x < tableSize; x++){
             System.out.println(table[x]);
         }
         System.out.println("");
