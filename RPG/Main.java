509bd9660048145ddce4a4114f5f7936bf2713e0
Mino <mino9421@gmail.com>
2021-03-29 22:20:53 -0400

    Update Main.java

--- RPG/Main.java
+++ RPG/Main.java
@@ -1,7 +1,16 @@
+/*
+    Authors: Armen Levon Armen - 101-281-931
+             Zion Henry        - 101-232-420
+*/
+
 package RPG;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        
+        System.out.println();
+
+    }
 }
 
 
@@ -29,5 +38,12 @@ public class Main {
                         - the amount of coins they have
                         - a list of all the items in their backpack
     6. exit
+
+
+    1)	A check should be made to see if an item with that name exits. 
+            a.	If it exists, the number of items of that type to add should be requested and added to the quantity in stock.
+            b.	If it does not exist, this means that a new item is being added to the shop and the following information must be requested:
+                i.	weaponName (String) ,  range (int), damage (int),  weight (double),  cost (double)
+                ii.	The quantity of the item being added must also be requested and the item must be created and added to the hash table.
 
 */
