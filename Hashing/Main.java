bc4ae9e0e489781bcfb8c6c8ba84e24f7a5d7c11
Mino <mino9421@gmail.com>
2021-04-06 20:01:52 -0400

    Update Main.java

--- Hashing/Main.java
+++ Hashing/Main.java
@@ -14,9 +14,11 @@ public class Main {
         // Account acc6 = new Account("araz", 123, "something");
         // Account acc7 = new Account("mino", 123, "something");
         // Account acc8 = new Account("talin", 123, "something");
-        Car acc8 = new Car("bmw", "m5", "123qwe", 123234);
+        Car car1 = new Car("bmw", "m5", "comment", 123234);
+        Car acc9 = new Car("bmw", "m5", "123qwe", 123234);
+        Car acc10 = new Car("bmw", "m5", "qweasd", 123234);
 
-        DoubleHashTable lht = new DoubleHashTable(); // current sample no arguments passed
+        DoubleHashTable lht = new DoubleHashTable(21, 0.75); // current sample no arguments passed
         
         // lht.insert(acc1);                         
         // lht.insert(acc2);                         //
@@ -25,11 +27,12 @@ public class Main {
         // lht.insert(acc5);                         //  
         // lht.insert(acc6);                         //  
         // lht.insert(acc7);                         //  
-        lht.insert(acc8);                         //  
-        lht.delete(acc8);
-        lht.insert(acc8);                         //
+        lht.insert(car1);                         //  
+        // lht.delete(car1);
+        lht.insert(acc9);                         //
+        lht.insert(acc10);                         //
         System.out.println(lht.search(acc8));
-        lht.update(acc8, 123);
+        // lht.update(acc8, 123);
         System.out.println(acc8.oilLevel);
         
         
